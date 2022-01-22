db9752fccef77927ad57bcfa9407fdecdc2aef4b
Ivan <ivanveselinov22@gmail.com>
2022-01-22 13:52:05 +1100

    Login Sign Up Done 100%

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,6 +2,14 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'http://localhost:4000' })
 
+// To check for users from middleware
+API.interceptors.request.use((req) => { 
+    if(localStorage.getItem('profile')){
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}` //get token from middleware
+    }
+    return req;
+});
+
 //read
 export const fetchPosts = () => API.get('/posts');
 //Create
