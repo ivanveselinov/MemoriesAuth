098e90bf072f16b26f4661c134774c048ff0de72
Ivan <ivanveselinov22@gmail.com>
2022-01-22 14:44:13 +1100

    Likes are working

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,6 +5,7 @@ import moment from 'moment';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import { useDispatch } from 'react-redux';
 import { deletePost, likePost } from '../../../actions/posts';
 
@@ -13,6 +14,22 @@ function Post({ post, setCurrentId }) {
     const classes = useStyles();
     const dispatch = useDispatch();
 
+    const user = JSON.parse(localStorage.getItem('profile'));
+
+    const Likes = () => {
+      if (post.likes.length > 0) {
+        return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+          ? (
+            <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
+          ) : (
+            <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
+          );
+      }
+  
+      return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+    };
+
+    
     return (
        <Card className={classes.card}>
                      {/* Image on Show */}
@@ -42,10 +59,9 @@ function Post({ post, setCurrentId }) {
                         {/* Card For Like and Delete  */}
            <CardActions className={classes.cardActions}>
                         {/* LIKE */}
-               <Button size="small" color="primary" onClick= {() => dispatch(likePost(post._id)) }>
+               <Button size="small" color="primary" disabled={!user?.result} onClick= {() => dispatch(likePost(post._id)) }>
                   <ThumbUpAltIcon fontSize="small" />
-                  &nbsp; Like &nbsp;
-                      {post.likeCount}
+                     <Likes/> {/* Import Component */}
                </Button>
                         {/* DELETE */}
                <Button size="small" color="primary" onClick= {() => dispatch(deletePost(post._id))}>
