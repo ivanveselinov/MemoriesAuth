1c0c7863c1c68ade10bce2eb5200f22594bdcc53
Ivan <ivanveselinov22@gmail.com>
2022-03-12 22:30:45 +1100

    Client deployed to Netlify

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:4000' })
+                                    // Heroku                                 // LocalHost
+const API = axios.create({ baseURL: 'https://memone.herokuapp.com/' || 'http://localhost:4000' })
 
 // To check for users from middleware
 API.interceptors.request.use((req) => { 
